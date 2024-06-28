f03689c4fcc29dafd907c7cc5043cb713f46ee81
Oleh Mykytiuk <oleh_mykytiuk@epam.com>
2024-06-28 13:18:37 +0300

    Add Aos service config schema
     - add pydantic models for Aos service config
     - add schema generation commands
     - add generated schema
     - add generated schema documentation
    
    Signed-off-by: Oleh Mykytiuk <oleh_mykytiuk@epam.com>

---
+++ src/cloud_common/schemas/service_config/aos_config.py
@@ -0,0 +1,159 @@
+#
+#  Copyright (c) 2018-2024 Renesas Inc.
+#  Copyright (c) 2018-2024 EPAM Systems Inc.
+#
+from datetime import datetime, timedelta
+from typing import Annotated, Optional, Literal, Dict
+
+from pydantic import BaseModel, Field
+
+from cloud_common.schemas.service_config.aos_types import UnitDevice, RunParameters, ServiceQuotas, AlertRules, \
+    ResourceRatios
+
+
+class AosConfigSchema(BaseModel):
+    """
+    Aos service config schema.
+
+    This schema describes the specification of the `aosService` layer in a service.
+    """
+
+    created: Annotated[
+        datetime,
+        Field(
+            alias='created',
+            description='Timestamp when Aos service was created.',
+        ),
+    ]
+
+    author: Annotated[
+        Optional[str],
+        Field(
+            alias='author',
+            default=None,
+            description='Aos service author.',
+        ),
+    ]
+
+    balancing_policy: Annotated[
+        Literal[
+            'enabled',
+            'disabled',
+        ],
+        Field(
+            alias='balancingPolicy',
+            default='enabled',
+            description='Balancing type. `disabled` means total prohibition from balancing to other nodes.`',
+        ),
+    ]
+
+    hostname: Annotated[
+        Optional[str],
+        Field(
+            alias='hostname',
+            default=None,
+            description='The hostname of the Aos service. The FQDN is {hostname].{service_provider}.',
+        ),
+    ]
+
+    runners: Annotated[
+        Optional[list[Literal['runc', 'crun', 'xrun']]],
+        Field(
+            alias='runners',
+            default=None,
+            description='Aos service allowed runner types. Absense means ["runc", "crun"].',
+        ),
+    ]
+
+    run_parameters: Annotated[
+        Optional[RunParameters],
+        Field(
+            alias='runParameters',
+            default=None,
+            description='Run parameters for the Aos service.',
+        ),
+    ]
+
+    offline_ttl: Annotated[
+        Optional[timedelta],
+        Field(
+            alias='offlineTTL',
+            default=None,
+            description="""\
+TTL (allowed time) to run service when unit in offline mode.
+If value is absent service will live on an unit forever.
+Format: ISO8601 duration.""",
+            examples=['PT1M', 'PT7D']
+        ),
+    ]
+
+    devices: Annotated[
+        Optional[UnitDevice],
+        Field(
+            alias='devices',
+            default=None,
+            description='List of needed or requested devices.'
+        ),
+    ]
+
+    resources: Annotated[
+        Optional[list[str]],
+        Field(
+            alias='resources',
+            default=None,
+            description='List of needed resources.',
+            examples=['bluetooth', 'system-dbus'],
+        )
+    ]
+
+    allowed_connections: Annotated[
+        Optional[list[dict]],
+        Field(
+            alias='allowedConnections',
+            default=None,
+            description="""\
+List of allowed network connections.
+Format of connection string: {service_uid}/[port|port_range]/[tcp|udp]""",
+            examples=[
+                '9931560c-be75-4f60-9abf-08297d905332/8087:8088/tcp',
+                '9931560c-be75-4f60-9abf-08297d905332/1515/udp',
+            ]
+        ),
+    ]
+
+    quotas: Annotated[
+        Optional[ServiceQuotas],
+        Field(
+            alias='quotas',
+            default=None,
+            description='Quotas for the service.'
+        ),
+    ]
+
+    resource_ratios: Annotated[
+        Optional[ResourceRatios],
+        Field(
+            alias='resourceRatios',
+            default=None,
+            description='Resource ratios for the service.'
+        ),
+    ]
+
+    alert_rules: Annotated[
+        Optional[AlertRules],
+        Field(
+            alias='alertRules',
+            default=None,
+            description='Alert rules for the service.'
+        ),
+    ]
+
+    permissions: Annotated[
+        Optional[Dict[str, Dict[str, Literal['r', 'rw', 'w']]]],
+        Field(
+            alias='permissions',
+            default=None,
+            description='Service permissions to access resources.',
+            examples=[{'vis': {'Signal.Doors.*': 'rw', 'Attributes.Vehicle.Vin': 'r'}}],
+        ),
+    ]
---
+++ src/cloud_common/schemas/service_config/aos_types.py
@@ -0,0 +1,265 @@
+#
+#  Copyright (c) 2018-2024 Renesas Inc.
+#  Copyright (c) 2018-2024 EPAM Systems Inc.
+#
+from datetime import timedelta
+from typing import Annotated, Optional
+
+from pydantic import BaseModel, Field
+
+
+class UnitDevice(BaseModel):
+    """Schema for the `device` info structure."""
+
+    name: Annotated[
+        str,
+        Field(
+            alias='name',
+            description="The name of the systems device.",
+            examples=['camera0', 'mic0'],
+        ),
+    ]
+
+    permissions: Annotated[
+        str,
+        Field(
+            alias='permissions',
+            description='The needed access permissions for the device.',
+            examples=['r', 'rw', 'w'],
+        ),
+    ]
+
+
+class RunParameters(BaseModel):
+    """Schema for startup parameters."""
+
+    start_interval: Annotated[
+        Optional[timedelta],
+        Field(
+            alias='startInterval',
+            default=None,
+            description='The duration in ISO8601 format to wait service start.',
+            examples=['PT10S', 'PT1M'],
+        ),
+    ]
+
+    start_burst: Annotated[
+        Optional[int],
+        Field(
+            alias='startBurst',
+            default=None,
+            description="""\
+Service which are started more than burst times within an interval time span are not permitted to start any more.
+Use `startInterval` to configure the checking interval and `startBurst`
+to configure how many starts per interval are allowed.""",
+            examples=[3, 10],
+        ),
+    ]
+
+    restart_interval: Annotated[
+        Optional[timedelta],
+        Field(
+            alias='restartInterval',
+            default=None,
+            description='The duration in ISO8601 format to wait before service restart.',
+            examples=['PT1S', 'PT1M'],
+        ),
+    ]
+
+
+class ResourceRatios(BaseModel):
+    """
+    Schema for resource ratios.
+
+    Each of specified resource ratio is treated as `requested` value.
+    """
+
+    cpu: Annotated[
+        Optional[float],
+        Field(
+            alias='cpu',
+            default=None,
+            description='CPU requested ratio in percents (against cpuLimit)',
+        ),
+    ]
+
+    ram: Annotated[
+        Optional[float],
+        Field(
+            alias='ram',
+            default=None,
+            description='RAM requested ratio in percents (against ramLimit)',
+        ),
+    ]
+
+    storage: Annotated[
+        Optional[float],
+        Field(
+            alias='storage',
+            default=None,
+            description='Storage requested ratio in percents (against storageLimit)',
+        ),
+    ]
+
+
+class ServiceQuotas(BaseModel):
+    """Schema for possible quotas for a service."""
+
+    cpu_limit: Annotated[
+        Optional[int],
+        Field(
+            alias='cpuLimit',
+            default=None,
+            description='CPU limit in DMIPs',
+        ),
+    ]
+
+    ram_limit: Annotated[
+        Optional[int],
+        Field(
+            alias='ramLimit',
+            default=None,
+            description='RAM limit in bytes',
+        ),
+    ]
+
+    storage_limit: Annotated[
+        Optional[int],
+        Field(
+            alias='storageLimit',
+            default=None,
+            description='Storage limit in bytes',
+        ),
+    ]
+
+    state_limit: Annotated[
+        Optional[int],
+        Field(
+            alias='stateLimit',
+            default=None,
+            description='State limit in bytes',
+        ),
+    ]
+
+    tmp_limit: Annotated[
+        Optional[int],
+        Field(
+            alias='tmpLimit',
+            default=None,
+            description='Temporary storage limit in bytes',
+        ),
+    ]
+
+    upload_speed: Annotated[
+        Optional[int],
+        Field(
+            alias='uploadSpeed',
+            default=None,
+            description='Upload limit in bytes per second',
+        ),
+    ]
+
+    download_speed: Annotated[
+        Optional[int],
+        Field(
+            alias='downloadSpeed',
+            default=None,
+            description='Upload limit in bytes per second',
+        ),
+    ]
+
+    files_limit: Annotated[
+        Optional[int],
+        Field(
+            alias='noFileLimit',
+            default=None,
+            description='Limit of opened files',
+        ),
+    ]
+
+    pids_limit: Annotated[
+        Optional[int],
+        Field(
+            alias='pidsLimit',
+            default=None,
+            description='Limit of PIDs',
+        ),
+    ]
+
+
+class AlertSettings(BaseModel):
+    """Schema alert triggering procedure."""
+
+    min_timeout: Annotated[
+        Optional[timedelta],
+        Field(
+            alias='minTimeout',
+            description='The duration in ISO8601 for a time window to check alert rule.',
+            examples=['PT10S', 'PT1M'],
+        ),
+    ]
+
+    min_threshold: Annotated[
+        Optional[int],
+        Field(
+            alias='minThreshold',
+            description='The minimum threshold to stop alert.',
+        ),
+    ]
+
+    max_threshold: Annotated[
+        Optional[int],
+        Field(
+            alias='maxThreshold',
+            description='The maximum threshold value to start alert.',
+        ),
+    ]
+
+
+class AlertRules(BaseModel):
+    """Schema for all possible alert rules."""
+
+    ram: Annotated[
+        Optional[AlertSettings],
+        Field(
+            alias='ram',
+            default=None,
+            description='RAM alert settings.',
+        ),
+    ]
+
+    cpu: Annotated[
+        Optional[AlertSettings],
+        Field(
+            alias='cpu',
+            default=None,
+            description='CPU alert settings.',
+        ),
+    ]
+
+    storage: Annotated[
+        Optional[AlertSettings],
+        Field(
+            alias='storage',
+            default=None,
+            description='Storage alert settings.',
+        ),
+    ]
+
+    upload: Annotated[
+        Optional[AlertSettings],
+        Field(
+            alias='upload',
+            default=None,
+            description='Upload alert settings.',
+        ),
+    ]
+
+    download: Annotated[
+        Optional[AlertSettings],
+        Field(
+            alias='download',
+            default=None,
+            description='Download alert settings.',
+        ),
+    ]
