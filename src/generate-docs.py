90ade3374902ea70b1c2e0b158b081a895d6ab00
Oleh Mykytiuk <oleh_mykytiuk@epam.com>
2024-07-11 12:56:52 +0300

    Add update format schema
     - add schema
     - add autogenerated doc
    
    Signed-off-by: Oleh Mykytiuk <oleh_mykytiuk@epam.com>

--- src/generate-docs.py
+++ src/generate-docs.py
@@ -6,6 +6,7 @@ import click
 
 from cloud_common.protocols.unit import base
 from cloud_common.schemas.service_config.aos_config import AosConfigSchema
+from cloud_common.schemas.unit_update.aos_update import AosUpdateSchema
 
 DOCS_BASE_DIR = os.path.abspath(os.path.join(
     os.path.dirname(__file__),
@@ -35,6 +36,16 @@ def generate_service_schema() -> str:
 
     return schema_filename
 
+
+def generate_update_schema() -> str:
+    schema = AosUpdateSchema.model_json_schema()
+    schema_filename = os.path.join(DOCS_BASE_DIR, 'aos-update.schema.json')
+    with open(schema_filename, 'wt') as file_handler:
+        file_handler.write(json.dumps(schema, indent=4))
+        file_handler.write('\n')
+    print(f'Schema generated at {schema_filename}')
+
+    return schema_filename
 
 @click.group()
 def cli():
@@ -90,23 +101,27 @@ def both():
 
 @cli.command()
 def schemas():
-    schema_filename = generate_service_schema()
+    schema_filenames = [
+        generate_service_schema(),
+        generate_update_schema(),
+    ]
 
-    subprocess.run([
-        'generate-schema-doc',
-        '--config-file',
-        os.path.join(DOCS_BASE_DIR, 'config-html.yaml'),
-        schema_filename,
-        os.path.join(DOCS_BASE_DIR, 'html'),
-    ])
+    for schema_filename in schema_filenames:
+        subprocess.run([
+            'generate-schema-doc',
+            '--config-file',
+            os.path.join(DOCS_BASE_DIR, 'config-html.yaml'),
+            schema_filename,
+            os.path.join(DOCS_BASE_DIR, 'html'),
+        ])
 
-    subprocess.run([
-        'generate-schema-doc',
-        '--config-file',
-        os.path.join(DOCS_BASE_DIR, 'config-md.yaml'),
-        schema_filename,
-        os.path.join(DOCS_BASE_DIR, 'md'),
-    ])
+        subprocess.run([
+            'generate-schema-doc',
+            '--config-file',
+            os.path.join(DOCS_BASE_DIR, 'config-md.yaml'),
+            schema_filename,
+            os.path.join(DOCS_BASE_DIR, 'md'),
+        ])
 
 
 if __name__ == '__main__':
