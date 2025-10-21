ae9474ab5f2b89adc42510b2af858b4c99589241
Oleh Mykytiuk <oleh_mykytiuk@epam.com>
2025-10-21 16:18:04 +0300

    Add ack/nack support to messages
    Signed-off-by: Oleh Mykytiuk <oleh_mykytiuk@epam.com>

---
+++ src/cloud_common/protocols/unit/v7/service_messages.py
@@ -0,0 +1,37 @@
+#
+#  Copyright (c) 2018-2025 EPAM Systems Inc.
+#
+from typing import Annotated, Literal
+
+from pydantic import BaseModel, Field
+
+
+class AosAckV7(BaseModel):
+    message_type: Annotated[
+        Literal['ack'],
+        Field(
+            alias='messageType',
+            title='Message type',
+            description='Message body type.',
+        ),
+    ]
+
+
+class AosNackV7(BaseModel):
+    message_type: Annotated[
+        Literal['nack'],
+        Field(
+            alias='messageType',
+            title='Message type',
+            description='Message body type.',
+        ),
+    ]
+
+    retry_after: Annotated[
+        int,
+        Field(
+            default=500,
+            alias='retryAfter',
+            description='Retry after time in milliseconds.',
+        )
+    ]
